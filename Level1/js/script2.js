c3a42a5d78254b9ff7e3b6d02a6e365e39205cc0
VoMinh88 <vhm.autotek@gmail.com>
2023-08-24 08:14:26 +0700

    a

--- Level1/js/script2.js
+++ Level1/js/script2.js
@@ -7,14 +7,13 @@
   });
   
 function xemThemBtn(){
- var newHeight =  $(".content-data")[0].offsetHeight;
-
- const height = 500;
- $(".content-data").css("height",height)
- $(".content-data").css('overflow',"hidden")
+ var newHeight =  $("#table-data-product")[0].offsetHeight;
+ $("#table-data-product").css("height",500)
+ $("#table-data-product").css('overflow',"hidden")
   $(".xemThemBtn").click(function (e) {
     e.preventDefault();
-    $(".content-data").css("height",newHeight)
+    $("#table-data-product").css({"height":newHeight,'overflow':"show"})
+    $("#xemThem").hide(); 
 })
 }
 
@@ -63,7 +62,7 @@ function loaddetail(){
             changeImg();
             dtproduct.forEach(el => {
               str=`
-              <table class="table text-start w-100" style ="word-wrap: break-word;"  >
+              <table class="table text-start w-100" style="background-color: rgb(190, 190, 231); ">
               <tbody>
                   <tr>
                       <td>Tên SP</td>
@@ -82,20 +81,36 @@ function loaddetail(){
                       <td>` +el.catename +`</td>
                   </tr>
                   <tr>
-                  <td>Giảm Giá</td>
-                  <td>`+el.discount+` %</td>
-              </tr>
-                  <tr>
-                      <td colspan="2" >`+el.content+`</td>
-  
-                  </tr>
+                    <td>Giảm Giá</td>
+                    <td>`+el.discount+` %</td>                  
+                  </tr>                 
               </tbody>
           </table>
-              `
+                <div class="row justify-content-between">
+                    <button class="btn btn-danger col m-2 buynowBtn" detail-id="`+el.id+`">Mua ngay</button> 
+                    <button class="btn btn-success col m-2 addtocartBtn" detail-id="`+el.id+`">Thêm Giỏ Hàng</button>   
+                </div>
+                <div class="row">
+                    <a href="detail.html?id=`+el.id+`" class="btn btn-primary col col-md m-2 w-100 chitietBtn" item-id="`+el.id+`">Chi tiết</a> 
+                </div>
+          
+          
+          `
               $("#table-product").html(str);
+              $(".content-data").hide(); 
+              $("#xemThem").hide(); 
               
+              $(".chitietBtn").click(function (e) { 
+                e.preventDefault();
+                $(".content-data").show();
+                $("#table-data-product").html(el.content)
+                // $("#xemThem").show();
+                // xemThemBtn();
+              });
             });
-            xemThemBtn();
+            
+            adddetailToCart()
+            
           }       
   // Kết thúc vòng ajax Thành công
         },
@@ -136,4 +151,36 @@ function loaddetail(){
       $('#changeimg-product').attr('src',src)
     });
     
+  }
+  function adddetailToCart() {
+    $('.addtocartBtn').click(function (e) { 
+      e.preventDefault();
+      if (localStorage.getItem('cart')&&localStorage.getItem('cart')!=null) {
+        
+        var cart = localStorage.getItem('cart')
+        var arr = JSON.parse(cart)
+      } else {
+        var arr = []
+      }
+      var id = Number($(this).attr('detail-id'))
+      var qty = 1
+  
+      var check = 0
+  
+      arr.forEach(el => {
+        if (id==el[0]) {
+          el[1]++
+          check = 1
+        } 
+      });
+      if(check==0) {
+        var item = [id,qty]
+        arr.push(item)
+      }
+      localStorage.setItem('cart',JSON.stringify(arr))
+        Toast.fire({
+          icon: "success",
+          title: "Đã Thêm Thành Công",
+        });
+    });
   }
