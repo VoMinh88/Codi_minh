0349d7f697451552aab0551370ed8e2e5e4ee867
VoMinh88 <108964505+VoMinh88@users.noreply.github.com>
2023-08-27 11:31:04 +0700

    giohang

--- final/js/home.js
+++ final/js/home.js
@@ -11,15 +11,12 @@ const Toast = Swal.mixin({
   },
 });
 const url = "https://students.trungthanhweb.com/api/home";
-
 var page = 1;
 ///////Khai báo Thông báo///////////
 $(document).ready(function () {
   login();
   logoutBtn();
   loadMenu();
-  loadProduct();
-  loadmoreProduct();  
   search();
 });
 
@@ -138,65 +135,7 @@ function loadMenu() {
     $("#cate-data").hide();
   }
 }
-function loadProduct() {
-  if (localStorage.getItem("token") && localStorage.getItem("token") != null) {
-    $.ajax({
-      type: "GET",
-      url: url,
-      data: {
-        apitoken: localStorage.getItem("token"),
-        page: page,
-      },
-      dataType: "JSON",
-      success: function (res) {
-        const product = res.products.data;
-        if (product.length > 0) {
-          var str = ``;
-          product.forEach((el) => {
-            str +=
-              `
-            <div class="col-md-3 mb-3" >
-              <div class="card border-info" style="border-radius: 10px; w-100">
-                <img src="https://students.trungthanhweb.com/images/`+el.images +`" class="card-img-top mt-2" alt="">
-              
-                <div class="card-body m-2">
-                  <h5 class="card-title">` +el.name +`</h5>
-                  <p class="card-text">Giá : ` +Intl.NumberFormat("en-US").format(el.price) +` vnđ</p>
-                  <p href="getCate.html?id=`+el.id+`">Loại sản phẩm : ` +el.catename +`</p>
-                  <p href="getBrandProducts.html?id=`+el.id+`">Loại sản phẩm : ` +el.brandname +`</p>
-                  <a href="detail.html?id=`+el.id+`" class="btn btn-primary col col-md m-2 w-auto chitietBtn" item-id="`+el.id+`">Chi tiết</a>
-                  <button class="btn btn-primary col col-md m-2 w-auto addtocartBtn" cart-id="`+el.id+`">Thêm Giỏ Hàng</button>                   
-                </div>
-              </div>
-            </div>
-            `;
-          });
 
-        }
-
-        if (res.products.next_page_url != null) {
-          page++;
-        } else {
-          $("#more-data").hide();
-        }
-
-        $("#product-data").append(str);
-        addToCart()
-      },
-    });
-  } else {
-    $("#body-data").hide();
-  }
-  
-
-}
-function loadmoreProduct() {
-  $("#more-data").click(function (e) {
-    e.preventDefault();
-    loadProduct();    
-  });
-
-}
 function addToCart() {
   $('.addtocartBtn').click(function (e) { 
     e.preventDefault();
--- final/js/loadcart.js
+++ final/js/loadcart.js
@@ -20,27 +20,42 @@ function loadCart() {
                     var sum=0;
                     res.result.forEach((el,index) => {
                         str+=`
-                        <tr class="">
+                        <tr class="table-primary">
                             <td scope="row">`+(++index)+`</td>
                             <td ><img src="`+el[3]+`" style="width:100px !important;height: auto; " alt=""></td>
                             <td>`+el[1]+`</td>
                             <td >`+Intl.NumberFormat('en-US').format(el[5])+`</td>
-                            <td>`+el[4]+`</td>
+                            <td ><input type="number" class="form-control editQty" data-id="`+el[0]+`" value="`+el[4]+`" min=0></td>
                             <td>`+Intl.NumberFormat('en-US').format(el[6])+`</td>
                         </tr>
                         `;
                         sum+=el[5];
+                        
                     });
                     str+=`
-                    <tr class="">
+                    <tr class="table-danger">
                         <td colspan='5' scope="row">Tổng tiền</td>
                         <td scope="row">`+Intl.NumberFormat('en-US').format(sum)+`</td>
 
                     </tr>
                     `
                     $("#cartressult").html(str);
+                    changeQty();
                 }
             }
         });
     }
+}
+
+function changeQty() {
+$('.editQty').change(function (e) { 
+    e.preventDefault();
+    var newQty = $(this).val()
+    var id = $(this).attr('data-id')
+    if (newQty==0) {
+        $('#DeleteModal').modal('show');
+    } else {
+        
+    }
+});
 }
